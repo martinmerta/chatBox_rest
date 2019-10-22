480afb0b53a4f7e0598cd7f9c4c28d23b16c7ca5
martinmerta <mmerta4@wp.pl>
2019-10-22 17:15:28 +0200

    add getMessage functionality

--- components/Message/MessageController.ts
+++ components/Message/MessageController.ts
@@ -1,6 +1,29 @@
-import {Request,Response, NextFunction} from 'express'
-
-export const getMessages = ( req:Request,res:Response,next:NextFunction)=>{}
-export const postMessages = ( req:Request,res:Response,next:NextFunction)=>{}
-export const putMessages = ( req:Request,res:Response,next:NextFunction)=>{}
-export const deleteMessages = ( req:Request,res:Response,next:NextFunction)=>{}
+import { Request, Response, NextFunction } from "express";
+import { messageSchema } from "./MessageModel";
+export const getMessages = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const fetchMessages = await messageSchema.find();
+    return res.status(200).json({ messages: fetchMessages });
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+};
+export const postMessages = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+export const putMessages = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+export const deleteMessages = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
